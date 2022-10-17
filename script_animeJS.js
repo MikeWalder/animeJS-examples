666f0c2472e7106de59847d58acf61306aab3d48
MikeWalder <mike_walder@hotmail.fr>
2022-10-17 23:21:41 +0200

    First animation tests

---
+++ script_animeJS.js
@@ -0,0 +1,22 @@
+anime({
+    targets: '.square',
+    translateX: 400,
+    translateY: 350,
+    rotateZ: 2160,
+    scale: 3.5,
+    duration: 3500,
+    easing: 'steps(150)',
+    borderRadius: ['0%', '50%'],
+    direction: 'alternate',
+    loop: true
+});
+
+anime({
+    targets: '.square2',
+    translateX: 550,
+    rotateZ: 360,
+    scale: 2.2, 
+    duration: 5000,
+    direction: 'alternate',
+    loop: true
+});
